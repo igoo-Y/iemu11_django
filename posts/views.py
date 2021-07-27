f78cda79733aa76afb446f82600204b1136d13fa
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-27 18:20:01 +0900

    CSS

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
-from django.db import models
+from django.db import connection, models
 from django.http import request
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls.base import reverse_lazy
 from django.views.generic import ListView, DetailView, DeleteView
 from django.urls import reverse_lazy
@@ -35,3 +35,18 @@ class DeletePostView(DeleteView):
     model = models.Post
     template_name = "auth/confirm_delete.html"
     success_url = reverse_lazy("posts:list")
+
+
+def posts_in_category(request, category_pk=None):
+    categories = models.Category.objects.all()
+    category_pk = request.get("category.pk")
+    posts = models.Post.objects.all()
+    if category_pk:
+        current_category = get_object_or_404(models.Category, category_pk=category_pk)
+        posts = models.Post.objects.filter(category=category_pk)
+    context = {
+        "categories": categories,
+        "current_category": current_category,
+        "posts": post,
+    }
+    return render(request, "posts/posts_list.html", context=context)
